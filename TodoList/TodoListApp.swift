a0e53b11ef24fa37eacd942ead1a068ec89aef89
Meezu Lawot <meezu.lawot@swifttech.com.np>
2023-08-22 00:32:29 +0545

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Meezu Lawot on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
